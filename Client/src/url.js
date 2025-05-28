3082bed4a16c9ff80b7ccd2d9a87531a9feb3963
HasnainAllahRakha <hasnainallahrakha58@gmail.com>
2025-05-28 12:58:14 +0500

    url change

--- Client/src/url.js
+++ Client/src/url.js
@@ -1,5 +1,6 @@
-const Base_Url = "http://localhost:5000/api";
-const Socket_Endpoint = "http://localhost:5000";
+const Base_Url = "http://13.53.182.243:5000/api";
+const Socket_Endpoint = "http://13.53.182.243:5000";
+
 const User_Signup = `${Base_Url}/user/register`;
 const User_Login = `${Base_Url}/user/login`;
 const User_Search = `${Base_Url}/user`;
@@ -10,5 +11,19 @@ const Chat_Group_Rename = `${Base_Url}/chat/group/rename`;
 const Chat_Group_Remove = `${Base_Url}/chat/group/remove`;
 const Chat_Group_add = `${Base_Url}/chat/group/add`;
 const Message_Create = `${Base_Url}/message`;
-const Fetch_Message = `${Base_Url}/message`
-export {Socket_Endpoint,User_Signup,User_Login,User_Search,Chat_Create,Chat_Fetch,Chat_Group_Create,Chat_Group_Rename,Chat_Group_Remove,Chat_Group_add,Message_Create,Fetch_Message}
+const Fetch_Message = `${Base_Url}/message`;
+
+export {
+  Socket_Endpoint,
+  User_Signup,
+  User_Login,
+  User_Search,
+  Chat_Create,
+  Chat_Fetch,
+  Chat_Group_Create,
+  Chat_Group_Rename,
+  Chat_Group_Remove,
+  Chat_Group_add,
+  Message_Create,
+  Fetch_Message
+};
